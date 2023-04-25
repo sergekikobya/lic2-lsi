40e47a1da9897ef186193207fad65ccd303ba545
sergekikobya <sergekikobya@gmail.com>
2023-04-25 10:48:04 +0200

    Codage index.php
    Utilisation de l'instruction echo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercices PHP</title>
+</head>
+</body>
+<?php echo"<font color='blue'><h1>
+<u>WELCOME TO LIC2-LSI</h1></u></font>";?>
+
+</body>
+</html>
